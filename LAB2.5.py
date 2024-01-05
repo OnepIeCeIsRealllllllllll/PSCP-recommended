33aea2650e988de662b838a094a6b5f9dc12620b
gam_gam <144825492+OnepIeCeIsRealllllllllll@users.noreply.github.com>
2024-01-05 12:20:36 +0700

    Add files via upload

--- LAB2.5.py
+++ LAB2.5.py
@@ -1,4 +1,3 @@
-"""Lab 02.04_Copy Stack"""
 class ArrayStack:
     """."""
     def __init__(self):
@@ -47,72 +46,27 @@ class ArrayStack:
     def print_stack(self):
         """พิมพ์ข้อมูลใน Stack"""
         return print(self.data)
-#เพิ่ม copy
-def copy_stack(stack1, stack2):
-    """บังคับ copy_stack(stack1"""
-    tmp = ArrayStack()
-    while not stack1.is_empty():
-        tmp.push(stack1.pop())
-    while not stack2.is_empty():
-        stack2.pop()
-    while not tmp.is_empty():
-        data = tmp.pop()
-        stack1.push(data)
-        stack2.push(data)
-#โจทย์
-def print_status():
-    """Print all stacks"""
-    print("This is stack 1 (%d): " % STACK1_.get_size(), end='')
-    STACK1_.print_stack()
-    print("This is stack 2 (%d): " % STACK2_.get_size(), end='')
-    STACK2_.print_stack()
-    print("This is stack 3 (%d): " % STACK3_.get_size(), end='')
-    STACK3_.print_stack()
-    print("This is stack 4 (%d): " % STACK4_.get_size(), end='')
-    STACK4_.print_stack()
-    print()
- 
- 
-STACK1_ = ArrayStack()
-STACK2_ = ArrayStack()
-STACK3_ = ArrayStack()
-STACK4_ = ArrayStack()
- 
-# เพิ่มข้อมูลใน Stack1
-for _ in range(int(input())):
-    STACK1_.push(input())
- 
-# เพิ่มข้อมูลใน Stack2
-for _ in range(int(input())):
-    STACK2_.push(input())
- 
-TEMP1_, TEMP2_, TEMP3_, TEMP4_ = id(STACK1_), id(
-    STACK2_), id(STACK3_), id(STACK4_)
- 
-print("Copy Stack 2 to Stack 4")
-copy_stack(STACK2_, STACK4_)
-print_status()
- 
-print("Copy Stack 1 to Stack 3")
-copy_stack(STACK1_, STACK3_)
-STACK1_.push("A")
-print_status()
- 
-print("Copy Stack 2 to Stack 1")
-copy_stack(STACK2_, STACK1_)
-STACK2_.push("B")
-print_status()
- 
-print("Copy Stack 3 to Stack 2")
-copy_stack(STACK3_, STACK2_)
-STACK3_.push("C")
-print("Copy Stack 1 to Stack 3")
-copy_stack(STACK1_, STACK3_)
-STACK1_.push("D")
-print("Copy Stack 2 to Stack 4")
-copy_stack(STACK2_, STACK4_)
-STACK2_.push("E")
-print_status()
- 
-print(TEMP1_ == id(STACK1_), TEMP2_ == id(STACK2_),
-      TEMP3_ == id(STACK3_), TEMP4_ == id(STACK4_))
+def is_parentheses_matching():
+    """จับคู่ ( และ )"""
+    stack_l = ArrayStack()
+    stack_r = ArrayStack()
+    stack_tmp = ArrayStack()
+    #pop = "None"
+    expression = input()
+    for i in expression:
+        if i == '(':
+            stack_l.push(i)
+            stack_tmp.push(i)
+        elif i == ')':
+            # pop = stack_tmp.pop()
+            # if pop == "None":
+            #     break
+            stack_tmp.pop()
+            stack_r.push(i)
+    if stack_l.size == stack_r.size and stack_tmp.is_empty():
+        print("Parentheses in", expression, "are matched")
+        return True
+    else:
+        print("Parentheses in", expression, "are unmatched")
+        return False
+print(is_parentheses_matching())
